e881983b05f73a2199581fa2a58bc1d9798fd9ab
Khawla192 <202005171@stu.uob.edu.bh>
2025-02-02 20:25:26 +0300

    building Pokemon app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css';
 import { useState } from 'react';
+import { Route, Routes } from 'react-router';
 import PokemonList from './components/PokemonList/PokemonList';
+import NavBar from './components/NavBar/NavBar';
+import PokemonDetails from './components/PokemonDetails/PokemonDetails';
+import PokemonForm from './components/PokemonForm/PokemonForm.jsx'
 
 const initialState = [
   { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
@@ -13,11 +17,25 @@ const initialState = [
 const App = () => {
 
   const [pokemon, setPokemon] = useState(initialState);
+  const addPokemon = (formData) => {
+    formData._id = pokemon.length + 1;
+    setPokemon([...pokemon, formData]);
+  };
 
   return (
     <>
-    <h1>Hello world!</h1>
-    <PokemonList pokemon={pokemon} />
+      <NavBar />
+      <h1>Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} />
+        <Route path="/pokemon/new" element={<PokemonForm addPokemon={addPokemon}/>} />
+        <Route
+          path="/pokemon/:pokemonId"
+          element={<PokemonDetails pokemon={pokemon} />}
+        />
+        <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
+      </Routes>
     </>
   )
 };
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,14 +1,18 @@
+import { Link } from 'react-router';
+
 const PokemonList = (props) => {
     return (
         <>
-        <h2>Pokemon</h2>
-        <ul>
-            {props.pokemon.map(poke => (
-                <li key={poke._id}>
-                    <h2>{poke.name}</h2>
-                </li>
-            ))}
-        </ul>
+            <h2>Pokemon</h2>
+            <ul>
+                {props.pokemon.map(poke => (
+                    <li key={poke.name}>
+                        <Link to={`/pokemon/${poke._id}`}>
+                            {poke.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 };
